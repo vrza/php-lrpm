6e3c165c41fc30ff65bf28701e6060b8b8cb4d30
Vladimir Vrzić <vvrzic@gmail.com>
2021-04-26 18:40:27 +0200

    Implement MockWorker->start()

--- src/MockWorker.php
+++ src/MockWorker.php
@@ -4,8 +4,13 @@ namespace PHPLRPM;
 
 class MockWorker implements Worker
 {
+    public function start()
+    {
+        fwrite(STDOUT, "MockWorker initialized" . PHP_EOL);
+    }
+
     public function cycle()
     {
-        fwrite(STDOUT, "MockWorker tick" . PHP_EOL);
+        fwrite(STDOUT, "MockWorker cycle" . PHP_EOL);
     }
 }
