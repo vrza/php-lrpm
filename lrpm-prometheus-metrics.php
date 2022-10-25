6147ac2387c6180d98c9cbde4a109912ed3b9d4b
Vladimir Vrzić <vvrzic@gmail.com>
2022-10-25 19:15:25 +0200

    OO serialisation of Prometheus metrics

--- lrpm-prometheus-metrics.php
+++ lrpm-prometheus-metrics.php
@@ -1,20 +1,76 @@
 <?php
 # php -S 127.0.0.1:8080 lrpm-prometheus-metrics.php
 
-$program = __DIR__ . '/bin/lrpmctl';
-$request = 'jsonstatus';
-$json = shell_exec("$program $request");
-$status = json_decode($json, true);
-$output = lrpm_status_to_prometheus_metrics($status);
-echo($output);
+class PrometheusMetric
+{
+    private $name;
+    private $value;
+    private $labels;
+    private $timestamp;
+
+    public function __construct(string $name, $value, array $labels = [], $timestamp = null)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->labels = $labels;
+        $this->timestamp = $timestamp;
+    }
+
+    public function __toString(): string
+    {
+        $output = $this->name;
+        if (!empty($this->labels)) {
+            $output .= '{';
+            $sep = '';
+            foreach ($this->labels as $labelName => $labelValue) {
+                $output .= $sep . $labelName . '=' . $labelValue;
+                $sep = ',';
+            }
+            $output .= '}';
+        }
+        $output .= ' ' . $this->value;
+        if (isset($this->timestamp)) $output .= ' ' . $this->timestamp;
+        return $output;
+    }
+}
+
+class PrometheusMetricsBuilder
+{
+    private $metrics = [];
+    private $text = '';
+
+    public function add(PrometheusMetric $metric): void
+    {
+        $this->metrics[] = $metric;
+    }
+
+    public function __toString(): string
+    {
+        foreach ($this->metrics as $metric) {
+            $this->text .= $metric->__toString() . PHP_EOL;
+        }
+        return $this->text;
+    }
+}
+
+function get_number_of_active_tasks(array $status): int
+{
+    $active_tasks = array_filter($status, function ($task) {
+        return isset($task['state']['pid']) && is_int($task['state']['pid']);
+    });
+    return count($active_tasks);
+}
 
 function lrpm_status_to_prometheus_metrics(array $status): string
 {
+    $metricsBuilder = new PrometheusMetricsBuilder();
+
     $taskCount = count($status);
+    $metricsBuilder->add(new PrometheusMetric('lrpm_task_count', $taskCount));
+
     $activeTaskCount = get_number_of_active_tasks($status);
     $inactiveTaskCount = $taskCount - $activeTaskCount;
-    $output = "lrpm_task_count $taskCount" . PHP_EOL;
-    $output .= "lrpm_inactive_task_count $inactiveTaskCount" . PHP_EOL;
+    $metricsBuilder->add(new PrometheusMetric('lrpm_inactive_task_count', $inactiveTaskCount));
 
     foreach ($status as $jobId => $jobStatus) {
         $config = $jobStatus['config'];
@@ -29,35 +85,18 @@ function lrpm_status_to_prometheus_metrics(array $status): string
                 'name' => $name
             ];
             if (isset($state['lastExitCode'])) $labels['last_exit_code'] = $state['lastExitCode'];
-            $metric = serialize_prometheus_metric('lrpm_task_backoff', $backoffDuration, $labels);
-            $output .= $metric . PHP_EOL;
+            $metricsBuilder->add(new PrometheusMetric('lrpm_task_backoff', $backoffDuration, $labels));
         }
     }
 
-    return $output;
+    return $metricsBuilder->__toString();
 }
 
-function get_number_of_active_tasks(array $status)
-{
-    $active_tasks = array_filter($status, function ($task) {
-        return isset($task['state']['pid']) && is_int($task['state']['pid']);
-    });
-    return count($active_tasks);
-}
+// main entry point
 
-function serialize_prometheus_metric(string $name, $value, array $labels = [], $timestamp = null): string
-{
-    $output = $name;
-    if (!empty($labels)) {
-        $output .= '{';
-        $sep = '';
-        foreach ($labels as $labelName => $labelValue) {
-            $output .= $sep . $labelName . '=' . $labelValue;
-            $sep = ',';
-        }
-        $output .= '}';
-    }
-    $output .= ' ' . $value;
-    if (isset($timestamp)) $output .= ' ' . $timestamp;
-    return $output;
-}
+$program = __DIR__ . '/bin/lrpmctl';
+$request = 'jsonstatus';
+$json = shell_exec("$program $request");
+$status = json_decode($json, true);
+$output = lrpm_status_to_prometheus_metrics($status);
+echo($output);
